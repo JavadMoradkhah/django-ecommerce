5e1d36117993ee83a8dff9eb9da3f2bfcd13cd5a
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-06-20 15:05:11 +0330

    feat: customize the user admin field sets

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from . import models
 
 admin.site.site_title = 'Administration'
 admin.site.site_header = 'Administration'
 admin.site.index_title = 'Dashboard'
 
-admin.site.register(models.User, UserAdmin)
+
+@admin.register(models.User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("first_name", "last_name", "username", "email", "password1", "password2"),
+            },
+        ),
+    )
