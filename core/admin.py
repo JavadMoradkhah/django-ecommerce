056f46fff38791527ec2f208ecfc34b38e23007e
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-06-20 14:39:05 +0330

    feat: set the admin site title, header and index title

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from . import models
 
+admin.site.site_title = 'Administration'
+admin.site.site_header = 'Administration'
+admin.site.index_title = 'Dashboard'
+
 admin.site.register(models.User, UserAdmin)
