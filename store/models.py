8acfbf7022e6323f98db20f9ac36e00c3f91946d
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-06-23 18:20:18 +0330

    feat: add the __str__ magic method to the tag model

--- store/models.py
+++ store/models.py
@@ -82,3 +82,6 @@ class Size(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
