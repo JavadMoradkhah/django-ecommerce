1674503a4f673becb05ec1e517e8a1589806f523
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-06-23 15:58:07 +0330

    feat: build the category model

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+LIST_PER_PAGE = 10
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'parent')
+    search_fields = ['name']
+    prepopulated_fields = {"slug": ["name"]}
+    autocomplete_fields = ['parent']
+    list_select_related = ['parent']
+    list_per_page = LIST_PER_PAGE
--- store/models.py
+++ store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    parent = models.ForeignKey(
+        'Category', on_delete=models.PROTECT,
+        related_name='children', null=True, blank=True
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
