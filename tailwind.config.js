878be09ed376b80c67c484438118d064a4288136
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-06-24 15:07:44 +0330

    feat: install and setup daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,22 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: '#111827',
+          secondary: '#2563eb',
+          accent: '#db2777',
+          neutral: '#f3f4f6',
+          'base-100': '#FFFFFF',
+          info: '#22d3ee',
+          success: '#16a34a',
+          warning: '#facc15',
+          error: '#f43f5e',
+        },
+      },
+    ],
+  },
 };
