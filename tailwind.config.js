69ddf57137f4241d7abfa85870c4c60c59d76030
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-07-20 22:41:29 +0330

    refactor: uninstall daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,19 @@
 module.exports = {
   content: ['./**/templates/**/*.html'],
   theme: {
-    extend: {},
-  },
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes: [
-      {
-        light: {
-          primary: '#111827',
-          secondary: '#2563eb',
-          accent: '#db2777',
-          neutral: '#f3f4f6',
-          'base-100': '#FFFFFF',
-          info: '#22d3ee',
-          success: '#16a34a',
-          warning: '#facc15',
-          error: '#f43f5e',
-        },
+    extend: {
+      colors: {
+        primary: '#111827',
+        secondary: '#2563eb',
+        accent: '#db2777',
+        neutral: '#f3f4f6',
+        'base-100': '#FFFFFF',
+        info: '#22d3ee',
+        success: '#16a34a',
+        warning: '#facc15',
+        error: '#f43f5e',
       },
-    ],
+    },
   },
+  plugins: [],
 };
