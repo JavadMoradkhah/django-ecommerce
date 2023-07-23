74ba778d6dfdcac526d99bf46e61c2312d9cb70f
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-07-23 14:53:11 +0330

    feat: install alpinejs

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   entry: {
     products: path.resolve(__dirname, './static/js/products.js'),
+    alpinejs: path.resolve(__dirname, './static/js/alpinejs.js'),
   },
   watch: true,
   devtool: false,
